a459b7bf888f3127431a20a66edaf9dbed613e2e
Ivan0x9 <ivmrsic@fesb.hr>
2017-07-20 13:04:07 +0200

    u login dodan warning message za validaciju - frontend

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -6,9 +6,17 @@ angular.module('myApp.controllers', []).
 controller('LoginCtrl', function ($scope, $window, socket) {
 
     $scope.sendLogin=function() {
-        //$window.alert($scope.usernameemail);
-        var data = {username : $scope.usernameemail, password : $scope.password};
-        socket.emit('send:login',data);
+
+        if($scope.usernameemail == undefined || $scope.password == undefined)
+        {
+            $scope.showMsg = { 'visibility': 'visible' };
+        }
+        else{
+            var data = {username : $scope.usernameemail, password : $scope.password};
+            socket.emit('send:login',data);
+
+            $scope.showMsg = { 'visibility': 'hidden' };
+        }
     }
 }).
   controller('AppCtrl', function ($scope, socket) {
