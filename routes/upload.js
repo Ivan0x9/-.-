5e50fc1c27370b73cfaf721d0744e37eb1aa9b13
Ivan0x9 <ivmrsic@fesb.hr>
2017-09-06 15:02:44 +0200

    upload v1.3 something

--- routes/upload.js
+++ routes/upload.js
@@ -31,7 +31,6 @@ router.post('/', function(req, res) {
                     var content = data;
 
                     var IBAN;
-
                     var id_rac;
 
 
@@ -43,23 +42,22 @@ router.post('/', function(req, res) {
 
                     IBAN = array[1].substr(18, 21);
 
-                    pool.query("SELECT id_br_rac FROM br_rac WHERE br_rac.IBAN ='" + IBAN + "';", function (error, results, fields) {
-                        if (error) throw error;
-                        if (results.length >= 0) {
-                            id_rac = results[0].id_br_rac;
+                    pool.getConnection(function(err, connection){
+                        if(err) throw err;
+                        connection.query("SELECT id_br_rac FROM br_rac WHERE IBAN ='"+IBAN+"';", function (error, results, fields) {
+                            if (error) throw error;
 
-                            wrapper(array,id_rac,newpath);
-                            res.redirect('/testtable');
-//res.write('success!');
-                            res.end();
-                        } else {
-                            res.end('wrong');
-                        }
+                            if (results.length < 0) {
+                                console.log(results);
+                                res.send("Dokument nije pravilno učitan.");
+                            } else {
+                                id_rac = results[0].id_br_rac;
+                                wrapper(array,id_rac,newpath);
+                                return res.redirect('/testtable');
 
-
+                            }
+                        });
                     });
-
-
 
                 });
                 // console.log(encoded);
@@ -73,78 +71,52 @@ router.post('/', function(req, res) {
 
 });
 
-
-
-
-
-
-
-
-
-
-    var wrapper = function(array,id_rac,newpath) {
-        var SQLHEAD = 'INSERT INTO transakcije' +
-            '(banka,racun,partner,adresa,grad,iznos,opis,datum,id_racun)' +
-            'VALUES';
-        var UI;
-        var temp;
-        var racun, partner, adresa, grad, iznos, opis, datum;
-
-        var BANKA;
-
-        BANKA = array[0].substr(7, 50).trim();
-
-        for (var i = 0; i < array.length; i++) {
-            if (array[i].charAt(999) == '5') {
-                temp = array[i].substr(0, 2);
-                if (temp.localeCompare('10') == 0) {
-                    UI = -1;
-
-                } else if (temp.localeCompare('20') == 0) {
-                    UI = 1;
-
-                }
-                racun = array[i].substr(2, 34).trim();
-                partner = array[i].substr(36, 70).trim();
-                adresa = array[i].substr(106, 35).trim();
-                grad = array[i].substr(141, 35).trim();
-                datum = array[i].substr(184, 8);
-                opis = array[i].substr(298, 140).trim();
-                iznos = parseInt(array[i].substr(227, 15));
-                iznos = iznos / 100;
-                iznos = iznos * UI;
-
-                SQLHEAD += "('" + BANKA + "','" + racun + "','" + partner + "','" + adresa + "','" + grad +
-                    "'," + iznos + ",'" + opis + "','" + datum + "'," + id_rac + '),';
-
-
+var wrapper = function(array,id_rac,newpath) {
+    var SQLHEAD = 'INSERT INTO transakcije' +
+        '(banka,racun,partner,adresa,grad,iznos,opis,datum,id_racun)' +
+        'VALUES';
+    var UI;
+    var temp;
+    var racun, partner, adresa, grad, iznos, opis, datum;
+    var BANKA;
+    BANKA = array[0].substr(7, 50).trim();
+    for (var i = 0; i < array.length; i++) {
+        if (array[i].charAt(999) == '5') {
+            temp = array[i].substr(0, 2);
+            if (temp.localeCompare('10') == 0) {
+                UI = -1;
+            } else if (temp.localeCompare('20') == 0) {
+                UI = 1;
             }
+            racun = array[i].substr(2, 34).trim();
+            partner = array[i].substr(36, 70).trim();
+            adresa = array[i].substr(106, 35).trim();
+            grad = array[i].substr(141, 35).trim();
+            datum = array[i].substr(184, 8);
+            opis = array[i].substr(298, 140).trim();
+            iznos = parseInt(array[i].substr(227, 15));
+            iznos = iznos / 100;
+            iznos = iznos * UI;
+            SQLHEAD += "('" + BANKA + "','" + racun + "','" + partner + "','" + adresa + "','" + grad +
+                "'," + iznos + ",'" + opis + "','" + datum + "'," + id_rac + '),';
         }
-        var fixedSQL = SQLHEAD.substring(0, SQLHEAD.length - 1);
-        fixedSQL += ';';
-        pool.query(fixedSQL, function (error, results, fields) {
-            if (error) throw error;
-            fs.unlink(newpath, function (err) {
-                if (err) return console.log(err);
-                //console.log('file deleted successfully');
-
-            });
-
-
-
+    }
+    var fixedSQL = SQLHEAD.substring(0, SQLHEAD.length - 1);
+    fixedSQL += ';';
+    pool.query(fixedSQL, function (error, results, fields) {
+        if (error) throw error;
+        fs.unlink(newpath, function (err) {
+            if (err) return console.log(err);
+            //console.log('file deleted successfully');
         });
-
-
-
-
-        /*
-         fs.writeFile('C:/Users/User2/Desktop/Nered/test.txt', fixedSQL, function (err) {
-             if (err) {
-                 return console.log(err);
-             }
-
-             console.log("The file was saved!");
-         });*/
-    };
+    });
+    /*
+        fs.writeFile('C:/Users/User2/Desktop/Nered/test.txt', fixedSQL, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("The file was saved!");
+        });*/
+};
 
 module.exports = router;
